47fcc55fb8c2ffaef2edb7c2aee4cbb84dfbfe35
chimiseanga <seang696@gmail.com>
2026-02-21 18:18:15 -0800

    Undo config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,22 +7,12 @@ import react from '@astrojs/react';
 
 import icon from 'astro-icon';
 
-import sanity from '@sanity/astro';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [
-      react(),
-      icon(),
-      sanity({
-        projectId: '0k7xpj4i', // Replace with your actual project ID
-        dataset: 'production', // Or your specific dataset name
-        useCdn: true,
-      }),
-  ],
+  integrations: [react(), icon()],
   output: "static",
 });
