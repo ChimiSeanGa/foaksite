d007c5b600298e3a93adf4f1224692f63b9d8e2a
chimiseanga <seang696@gmail.com>
2026-02-21 18:13:28 -0800

    Fixed config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,22 @@ import react from '@astrojs/react';
 
 import icon from 'astro-icon';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), icon()],
+  integrations: [
+      react(),
+      icon(),
+      sanity({
+        projectId: '0k7xpj4i', // Replace with your actual project ID
+        dataset: 'production', // Or your specific dataset name
+        useCdn: true,
+      }),
+  ],
   output: "static",
 });
